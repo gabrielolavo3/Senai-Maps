8fb84c8af706f2e7ea9d6c9d134a1994ab94618d
Gabriel S. Olavo <143572039+gabrielolavo3@users.noreply.github.com>
2024-06-11 02:34:13 -0300

    Add files via upload

--- senaiMaps/js/script.js
+++ senaiMaps/js/script.js
@@ -20,16 +20,50 @@ const primeiraFoto = document.querySelector('#foto-1')
 const segundaFoto = document.querySelector('#foto-2')
 const terceiraFoto = document.querySelector('#foto-3')
 
-// Eventos
+// Eventos e Functions
+
+blocoA.addEventListener ('click', primeiro)
+blocoB.addEventListener ('click', segundo)
+
+function primeiro() {
+    tituloDesc.textContent = 'Bloco A - Setor de Gerência e Edificação'
+    descricao.textContent = 'Localização: Em cima da praça de alimentação, basta subir as escadas'
+    primeiraFoto.src = 'images/bloco_A.jpg'
+    segundaFoto.src = 'images/bloco_AA.jpg'
+}
+
+function segundo() {
+    tituloDesc.textContent = 'Bloco B - Laboratório de Informatica'
+    descricao.textContent = 'Localização: Atrás do bloco A. Siga o caminho à direita do bloco A'
+    primeiraFoto.src = 'images/bloco_B.jpg'
+    segundaFoto.src = 'images/bloco_BB.jpg'
+} 
+
+blocoC.addEventListener ('click', terceiro)
+blocoD.addEventListener ('click', quarto)
+
+function terceiro() {
+    tituloDesc.textContent = 'Bloco C - Laboratório de Gráfica'
+    descricao.textContent = 'Localização: Lado esquerdo do bloco A '
+    primeiraFoto.src = 'images/bloco_c.jpg'
+    segundaFoto.src = ''
+    terceiraFoto.src = ''
+}
+
+function quarto() {
+    tituloDesc.textContent =' Bloco D - Laboratório de Automação'
+    descricao.textContent = 'Localização: No primeiro estacionamento, em frente ao bloco C ' 
+    primeiraFoto.src = 'images/bloco_d.jpg'
+    segundaFoto.src = 'images/bloco_dd.jpg'
+    terceiraFoto.src = ''
+}
 
 blocoE.addEventListener ('click', cinco)
 blocoF.addEventListener ('click', sexto)
 
-//Funções
-
 function cinco() {
     tituloDesc.textContent = `Bloco E - Setor Administrativo`
-    descricao.textContent = `Onde fica o CAA, em frente ao bloco A, junto a portaria`
+    descricao.textContent = `Localização: Onde fica o CAA, em frente ao bloco A, junto a portaria`
     primeiraFoto.src = 'images/bloco_e.jpg'
     segundaFoto.src = ''
     terceiraFoto.src = ''
@@ -37,8 +71,57 @@ function cinco() {
 
 function sexto() {
     tituloDesc.textContent = `Bloco F - Laboratório de Automação`
-    descricao.textContent = `Ao lado esquerdo do bloco E` 
+    descricao.textContent = `Localização: Lado esquerdo do bloco E` 
     primeiraFoto.src = 'images/bloco_f.jpg'
     segundaFoto.src = ''
     terceiraFoto.src = ''
+}
+
+blocoG.addEventListener ('click', sete)
+blocoH.addEventListener ('click', oito)
+
+//Funções
+
+function sete() {
+    tituloDesc.textContent = 'Bloco G - Laboratório de Eletrônica'
+    descricao.textContent = 'Localização: Lado esquerdo do F, Em frente ao estacionamento de motos'
+    primeiraFoto.src = 'images/bloco_g1.jpg'
+    segundaFoto.src = 'images/bloco_g2.jpg'
+    terceiraFoto.src = ''
+}
+
+function oito() {
+    tituloDesc.textContent = 'Bloco H - Laboratório de Elétrica'
+    descricao.textContent = 'Localização: Lado esquerdo do bloco G, junto das placas solares '
+    primeiraFoto.src = 'images/bloco_h.jpg'
+    segundaFoto.src = ''
+    terceiraFoto.src = ''
+}
+
+blocoL.addEventListener ('click', decimoSeg)
+blocoM.addEventListener ('click', decimoTer)
+blocoN.addEventListener ('click', decimoQua)
+
+function decimoSeg(){
+    tituloDesc.textContent = 'Bloco L - Setor de Exposição'
+    descricao.textContent = 'Localização: Lado esquerdo do bloco K'
+    primeiraFoto.src = 'images/bloco_l.jpg'
+    segundaFoto.src = ''
+    terceiraFoto.src = ''
+}
+
+function decimoTer(){
+    tituloDesc.textContent= 'Bloco M - Setor de Alimentos e Bebidas'
+    descricao.textContent= 'Localização: Lado direito do bloco J, local que fica o refeitório e a biblioteca'
+    primeiraFoto.src='images/bloco_m.jpg'
+    segundaFoto.src=''
+    terceiraFoto.src=''
+}
+
+function decimoQua(){
+    tituloDesc.textContent= 'Bloco N - Laboratório de Construção Civil'
+    descricao.textContent= 'Localização: Em frente ao bloco M, no segundo estacionamento'
+    primeiraFoto.src='images/bloco_n.jpg'
+    segundaFoto.src=''
+    terceiraFoto.src=''
 }
